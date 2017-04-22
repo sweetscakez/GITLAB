f3f7c3fc4ae7f4d39a5f8a37dd4493bc47099cf0
omosama <kamomo004@gmail.com>
2017-04-22 15:08:29 +0700

    modify program1

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,7 +5,7 @@ void main()
   int i;
   do
   {
-    printf("What is your name? ");
+    printf("What's your name? ");
     scanf("%s", Name);
     do
     {
@@ -44,5 +44,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye\n");
+  printf("See you again\n");
 }
