2587495fb00de9b5a2717d305e63b15e2d50cb09
noyRK <rksunnynoy@gmail.com>
2017-04-22 15:08:54 +0700

    Modified by noy

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,7 +5,7 @@ void main()
   int i;
   do
   {
-    printf("What is your name? ");
+    printf("What is your name?? ");
     scanf("%s", Name);
     do
     {
@@ -44,5 +44,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye\n");
+  printf("Goodbyee\n");
 }
